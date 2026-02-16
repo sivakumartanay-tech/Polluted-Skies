cbbbc45527b490c2dbf7581c1d69e7eb0de5f085
sivakumartanay-tech <sivakumartanay@gmail.com>
2026-02-17 01:38:30 +0800

    Add files via upload

--- player_1.gd
+++ player_1.gd
@@ -3,7 +3,10 @@ extends CharacterBody2D
 
 const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
+
 var move = true
+var jumping = false
+var max_air_jups = 0
 @onready var player: AnimatedSprite2D = $AnimatedSprite2D
 
 
@@ -13,9 +16,15 @@ func _physics_process(delta: float) -> void:
 		if not is_on_floor():
 			velocity += get_gravity() * delta
 
-		# Handle jump.
 		if Input.is_action_just_pressed("Jump") and is_on_floor():
 			velocity.y = JUMP_VELOCITY
+			max_air_jups = 1
+		elif Input.is_action_just_pressed("Jump") and max_air_jups > 0:
+			max_air_jups -= 1
+			jumping = true
+			velocity.y = JUMP_VELOCITY
+		if is_on_floor():
+			jumping = false
 
 		# Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
@@ -30,8 +39,10 @@ func _physics_process(delta: float) -> void:
 		else:
 			player.flip_h = false
 		
-		if not is_on_floor():
-			player.play("new_animation")
+		if not is_on_floor() and not jumping:
+			player.play("Jump")
+		elif jumping:
+			player.play("Air jump")
 		else:
 			if direction != 0 and move:
 				player.play("Run")
