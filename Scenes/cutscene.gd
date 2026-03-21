1f7c9ce7f3b41674d1fd1bb379dd03c5b6c763b3
Ashkesh-D <ashlesh.deshmukh.sg@gmail.com>
2026-03-21 19:26:41 +0800

    upd for changes friend made on my laptop

---
+++ Scenes/cutscene.gd
@@ -0,0 +1,8 @@
+extends Node
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
---
+++ Scripts/kill_area.gd
@@ -0,0 +1,16 @@
+extends Area2D
+@onready var timer: Timer = $Timer
+
+
+func _on_body_entered(_body: Node2D) -> void:
+	if _body.is_in_group("Players"):
+		print("died")
+		Engine.time_scale = 2
+		_body.get_node("CollisionShape2D").queue_free()
+		timer.start()
+
+
+func _on_timer_timeout() -> void:
+	Engine.time_scale = 1
+	get_tree().reload_current_scene() 
+	Global.money = 0
